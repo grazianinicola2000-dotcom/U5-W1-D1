1874c8cdec0e066db223df35e79ac27e3126b6e0
Graziani Nicola <graziani.nicola2000@gmail.com>
2026-04-09 15:43:55 +0200

    U5-W1-D3 tests for Pizza and Order

--- src/test/java/nicolagraziani/U5_W1_D1/U5W1D1ApplicationTests.java
+++ src/test/java/nicolagraziani/U5_W1_D1/U5W1D1ApplicationTests.java
@@ -1,13 +1,66 @@
 package nicolagraziani.U5_W1_D1;
 
+import nicolagraziani.U5_W1_D1.entities.Menu;
+import nicolagraziani.U5_W1_D1.entities.Order;
+import nicolagraziani.U5_W1_D1.entities.Pizza;
+import nicolagraziani.U5_W1_D1.entities.Topping;
+import nicolagraziani.U5_W1_D1.enums.OrderState;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalTime;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class U5W1D1ApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    @Qualifier("salami_pizza")
+    private Menu pizzaSalami;
+    @Autowired
+    @Qualifier("hawaiian_pizza")
+    private Menu pizzaHawaiian;
+    @Autowired
+    @Qualifier("lemonade")
+    private Menu lemonade;
+    @Autowired
+    @Qualifier("cheese")
+    private Topping cheese;
+    @Autowired
+    @Qualifier("ham")
+    private Topping ham;
+    @Autowired
+    @Qualifier("tomato")
+    private Topping tomato;
+    @Value("${seats.price}")
+    private double seatPrice;
+
+
+    @Test
+    public void testOrderCreationAndTotalPrice() {
+        Order order = new Order(List.of(pizzaHawaiian, pizzaSalami, lemonade), 1, OrderState.SERVITO, 5, LocalTime.now(), seatPrice);
+        assertNotNull(order);
+        assertEquals(29.04, order.getTotal());
+    }
+
+    @Test
+    public void testSetPizzaPrice() {
+        Pizza pizza = new Pizza("Prosciutto", List.of(ham, tomato, cheese));
+        double pizzaPrice = pizza.getPrice();
+        assertEquals(5.98, pizzaPrice);
+    }
+
+    @Test
+    public void testSetPizzaCalories() {
+        Pizza pizza = new Pizza("Prosciutto", List.of(ham, tomato, cheese));
+        int pizzaCalories = pizza.getCalories();
+        assertEquals(1139, pizzaCalories);
+    }
 
 }
